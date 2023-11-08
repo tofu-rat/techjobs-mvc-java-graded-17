d810141d900156a20a1bcdf1f4799e05c23e10c1
tofu-rat <bbseitan@gmail.com>
2023-11-08 16:38:32 -0600

    task3

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.launchcode.techjobsmvc.models.JobData;
+import java.util.ArrayList;
+import org.launchcode.techjobsmvc.models.Job;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
@@ -23,5 +27,26 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
+    @GetMapping("/search/results")
+    public String displaySearchResults(Model model,
+                                       @RequestParam String searchType,
+                                       @RequestParam String searchTerm) {
+
+        ArrayList<Job> jobs;
+
+        if ("all".toLowerCase().equals(searchTerm.toLowerCase()) || searchTerm.isEmpty()) {
+            jobs = JobData.findAll();
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
+
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", ListController.columnChoices);
+
+        return "search";
+    }
 }
+
+
 
